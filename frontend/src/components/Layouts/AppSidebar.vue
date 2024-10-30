336e3fe3c2472cdba42c3a93d8238df89e381e92
Rajeshbala2020 <rajeshbala.qbs@gmail.com>
2024-10-30 16:13:57 +0530

    Sidmenu help link

--- frontend/src/components/Layouts/AppSidebar.vue
+++ frontend/src/components/Layouts/AppSidebar.vue
@@ -71,6 +71,37 @@
               class="mx-2 my-0.5"
             />
           </nav>
+          <a
+            href="https://docs.bizpoleapps.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            :class="[
+              'absolute bottom-11 mt-auto flex items-center px-[14px] py-1 text-sm text-gray-700',
+              !isSidebarCollapsed ? 'left-[2px]' : 'left-0',
+            ]"
+          >
+            <span class=""
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
+                />
+              </svg>
+            </span>
+            <span
+              v-if="!isSidebarCollapsed"
+              class="flex-1 flex-shrink-0 truncate text-sm duration-300 ease-in-out ml-2 w-auto opacity-100"
+              >Help</span
+            >
+          </a>
         </Section>
       </div>
     </div>
