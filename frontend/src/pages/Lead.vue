8eb643e066c2a3dd52776299e0de5abc5add606b
Rajeshbala2020 <rajeshbala.qbs@gmail.com>
2024-11-11 16:30:01 +0530

    bug fixes

--- frontend/src/pages/Lead.vue
+++ frontend/src/pages/Lead.vue
@@ -36,6 +36,7 @@
       <Button
         :label="__('Convert to Deal')"
         variant="solid"
+        v-if="lead.data.status == 'Qualified'"
         class="bg-btn_primary"
         @click="showConvertToDealModal = true"
       />
