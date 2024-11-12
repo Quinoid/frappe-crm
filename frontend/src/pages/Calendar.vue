f286955b0506637948b4d3ad5df428c56fba89cd
Rajeshbala2020 <rajeshbala.qbs@gmail.com>
2024-11-12 15:14:48 +0530

    bbug fixes

--- frontend/src/pages/Calendar.vue
+++ frontend/src/pages/Calendar.vue
@@ -1,58 +1,71 @@
 <template>
-  <LayoutHeader>
-    <template #left-header>
-      <Breadcrumbs
-        :items="[{ label: 'Calendar', route: { name: 'Calendar' } }]"
-      />
-    </template>
-  </LayoutHeader>
-  <div
-    v-if="!isLoading"
-    class="flex flex-col h-full m-5 p-5 shadow-sm rounded-sm"
-  >
-    <Calendar :config="config" :events="calendarEvents">
-      <template
-        #header="{
-          currentMonthYear,
-          enabledModes,
-          activeView,
-          updateActiveView,
-        }"
-      >
-        <div v-if="calendarEvents.length === 0" class="text-center">
-          No events to display.
-        </div>
-        <div class="flex justify-between items-center mb-4 gap-3">
-          <div class="flex gap-3 items-center">
-            <select
-              v-model="selectedMonth"
-              class="px-2 py-1 border rounded-md text-lg font-bold"
-              @change="handleMonthChange"
-            >
-              <option
-                v-for="(month, index) in months"
-                :key="index"
-                :value="index"
-              >
-                {{ month }}
-              </option>
-            </select>
-            <span class="font-bold">{{ currentYear }}</span>
-          </div>
-          <div class="flex gap-2 text-sm">
-            <button
-              v-for="mode in enabledModes"
-              :key="mode"
-              class="px-2 py-1 rounded-md hover:bg-gray-100 transition-all duration-300 ease-in-out"
-              :class="{ 'font-bold': mode.label === activeView }"
-              @click="updateActiveView(mode.label)"
-            >
-              {{ mode.label }}
-            </button>
-          </div>
-        </div>
+  <div class="contents">
+    <LayoutHeader>
+      <template #left-header>
+        <Breadcrumbs
+          :items="[{ label: 'Calendar', route: { name: 'Calendar' } }]"
+        />
       </template>
-    </Calendar>
+    </LayoutHeader>
+    <div
+      v-if="!isLoading"
+      class="flex flex-col h-full m-5 p-5 shadow-sm rounded-sm"
+    >
+      <Calendar
+        :config="config"
+        :events="calendarEvents"
+        :create="createEvent"
+        :update="updateEvent"
+        :delete="deleteEvent"
+        @dblclick="openCreateEventModal"
+        :showCreateEventButton="true"
+      >
+        <template
+          #header="{
+            enabledModes,
+            activeView,
+            updateActiveView,
+            createNewEventClick,
+          }"
+        >
+          <div class="flex justify-between items-center mb-4 gap-3">
+            <div class="flex gap-3 items-center">
+              <select
+                v-model="selectedMonth"
+                class="px-2 py-1 border rounded-md !border-[#e8dcdc] text-lg font-bold min-w-[125px]"
+                @change="handleMonthChange"
+              >
+                <option
+                  v-for="(month, index) in months"
+                  :key="index"
+                  :value="index"
+                >
+                  {{ month }}
+                </option>
+              </select>
+              <span class="font-bold">{{ currentYear }}</span>
+            </div>
+            <div class="flex gap-2 text-sm">
+              <button
+                v-for="mode in enabledModes"
+                :key="mode"
+                class="px-2 py-1 rounded-md hover:bg-gray-100 transition-all duration-300 ease-in-out"
+                :class="{ 'font-bold': mode.label === activeView }"
+                @click="updateActiveView(mode.label)"
+              >
+                {{ mode.label }}
+              </button>
+              <button
+                @click="createNewEventClick"
+                class="px-2 py-1 font-semibold text-white bg-btn_primary rounded-md hover:bg-primary-600 transition-all duration-300 ease-in-out"
+              >
+                Create Event
+              </button>
+            </div>
+          </div>
+        </template>
+      </Calendar>
+    </div>
   </div>
 </template>
 
@@ -61,11 +74,18 @@ import { ref, computed, watch } from 'vue'
 import { Calendar, createResource } from 'qbs-vue-ui'
 import LayoutHeader from '@/components/LayoutHeader.vue'
 import { Breadcrumbs } from 'qbs-vue-ui'
+import { createToast } from '@/utils'
 export default {
   components: { Calendar, LayoutHeader, Breadcrumbs },
   setup() {
     const isLoading = ref(true)
     const rawEvents = ref([])
+    const isCreateEventModalOpen = ref(false) // Track modal visibility
+
+    const openCreateEventModal = () => {
+      isCreateEventModalOpen.value = true
+    }
+
     const config = ref({
       disableModes: [],
       defaultMode: 'Month',
@@ -73,6 +93,7 @@ export default {
       eventIcons: {},
       redundantCellHeight: 50,
       hourHeight: 50,
+      isDeletable: false,
       enableShortcuts: true,
     })
 
@@ -110,6 +131,14 @@ export default {
       currentMonth.value,
       currentYear.value,
     )
+    const createEventResource = createResource({
+      url: 'crm.api.events.custom_create_event',
+      method: 'post',
+    })
+    const updateEventResource = createResource({
+      url: 'crm.api.events.custom_edit_event',
+      method: 'post',
+    })
 
     const eventsResource = createResource({
       url: 'crm.api.events.custom_list_events',
@@ -167,6 +196,84 @@ export default {
         ends_on: getMonthRange(currentMonth.value, currentYear.value).ends_on,
       })
     }
+    function appendTimeToDate(dateStr, timeStr) {
+      // Format the Date object to "YYYY-MM-DD HH:MM:SS"
+      const formattedDate = `${dateStr} ${timeStr}`
+
+      return formattedDate
+    }
+    function transformEvent(event, update) {
+      const { title, date, from_time, color, to_time, id, ...rest } = event
+      console.log(event)
+      // Create a new object with the transformed keys
+      const transformedEvent = {
+        subject: title, // title to subject
+        starts_on: appendTimeToDate(date, from_time), // date to starts_on
+        ends_on: appendTimeToDate(date, to_time), //
+        custom_color: color, // color to custom_color
+        from_time: from_time,
+        name: update ? id : '',
+        ...rest, // include the remaining properties as is
+      }
+
+      return transformedEvent
+    }
+
+    const createEvent = async (event) => {
+      try {
+        const newEvent = await createEventResource.fetch({
+          ...transformEvent(event),
+        })
+        handleMonthChange() // Ensure this re-fetches the events
+        createToast({
+          title: newEvent.message,
+          icon: 'check',
+          iconClasses: 'text-green-600',
+        })
+        isCreateEventModalOpen.value = false // Close modal after creation
+      } catch (error) {
+        console.error('Error creating event:', error)
+      }
+    }
+
+    const updateEvent = async (event) => {
+      try {
+        const newEvent = await updateEventResource.fetch({
+          ...transformEvent(event, true),
+        })
+
+        await eventsResource.refresh()
+        isCreateEventModalOpen.value = false // Close modal after creation
+        handleMonthChange() // Ensure this re-fetches the events
+        createToast({
+          title: newEvent.message,
+          icon: 'check',
+          iconClasses: 'text-green-600',
+        })
+        console.log('Event updated:', newEvent)
+      } catch (error) {
+        console.error('Error creating event:', error)
+      }
+    }
+
+    const deleteEvent = async (event) => {
+      try {
+        const response = await fetch(
+          `crm.api.events.delete_event/${event.id}`,
+          {
+            method: 'DELETE',
+          },
+        )
+        if (response.ok) {
+          eventsResource.refresh()
+          console.log('Event deleted:', event.id)
+        } else {
+          console.error('Failed to delete event')
+        }
+      } catch (error) {
+        console.error('Error deleting event:', error)
+      }
+    }
 
     return {
       calendarEvents,
@@ -176,6 +283,10 @@ export default {
       selectedMonth,
       months,
       handleMonthChange,
+      deleteEvent,
+      updateEvent,
+      createEvent,
+      openCreateEventModal,
     }
   },
 }
