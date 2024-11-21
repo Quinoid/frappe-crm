003b8cc720a2948952640b9bef426901154b77ca
Arya <Arya-Dilipan>
2024-11-21 14:05:33 +0530

    adds communication custom list api

--- crm/api/communication.py
+++ crm/api/communication.py
@@ -77,5 +77,348 @@ def is_password_set():
 
 
 
+@staticmethod
+def custom_communication_default_list_data():
+    columns = [
+        {
+            'label': 'Name',
+            'type': 'Data',
+            'key': 'name',
+            'width': '12rem',
+        },
+        {
+            'label': 'Subject',
+            'type': 'Data',
+            'key': 'subject',
+            'width': '10rem',
+        },
+        {
+            'label': 'Content',
+            'type': 'Data',
+            'key': 'content',
+            'width': '8rem',
+        },
+        {
+            'label': 'Sender',
+            'type': 'Data',
+            'key': 'sender',
+            'width': '8rem',
+        },
 
+        {
+            'label': 'Recipients',
+            'type': 'Data',
+            'key': 'recipients',
+            'width': '8rem',
+        },
+
+        {
+            'label': 'Cc',
+            'type': 'Data',
+            'key': 'cc',
+            'width': '8rem',
+        },
+
+        {
+            'label': 'Bcc',
+            'type': 'Data',
+            'key': 'bcc',
+            'width': '8rem',
+        },
+
+        {
+            'label': 'Communication Date',
+            'type': 'Data',
+            'key': 'communication_date',
+            'width': '8rem',
+        },
+
+
+        {
+            'label': 'Last Modified',
+            'type': 'Datetime',
+            'key': 'modified',
+            'width': '8rem',
+        },
+    ]
+    rows = [
+        "name",
+        "subject",
+        "content",
+        "sender",
+        "recipients",
+        "cc",
+        "bcc",
+        "communication_date",
+        "modified",
+    ]
+    return {'columns': columns, 'rows': rows}
+
+
+@frappe.whitelist()
+def get_communication_data(
+    doctype: str,
+    filters: dict,
+    order_by: str,
+    page_length=20,
+    page_length_count=20,
+    column_field=None,
+    title_field=None,
+    columns=[],
+    rows=[],
+    kanban_columns=[],
+    kanban_fields=[],
+    view=None,
+    default_filters=None,
+):
+    custom_view = False
+    filters = frappe._dict(filters)
+    rows = frappe.parse_json(rows or "[]")
+    columns = frappe.parse_json(columns or "[]")
+    kanban_fields = frappe.parse_json(kanban_fields or "[]")
+    kanban_columns = frappe.parse_json(kanban_columns or "[]")
+
+    custom_view_name = view.get('custom_view_name') if view else None
+    view_type = view.get('view_type') if view else None
+    group_by_field = view.get('group_by_field') if view else None
+
+    for key in filters:
+        value = filters[key]
+        if isinstance(value, list):
+            if "@me" in value:
+                value[value.index("@me")] = frappe.session.user
+            elif "%@me%" in value:
+                index = [i for i, v in enumerate(value) if v == "%@me%"]
+                for i in index:
+                    value[i] = "%" + frappe.session.user + "%"
+        elif value == "@me":
+            filters[key] = frappe.session.user
+
+    if default_filters:
+        default_filters = frappe.parse_json(default_filters)
+        filters.update(default_filters)
+
+    is_default = True
+    data = []
+    _list = get_controller(doctype)
+    default_rows = []
+    if hasattr(_list, "custom_communication_default_list_data"):
+        default_rows = custom_communication_default_list_data().get("rows")
+
+    if view_type != "kanban":
+        if columns or rows:
+            custom_view = True
+            is_default = False
+            columns = frappe.parse_json(columns)
+            rows = frappe.parse_json(rows)
+
+        if not columns:
+            columns = [
+                {"label": "Name", "type": "Data", "key": "name", "width": "16rem"},
+                {"label": "Last Modified", "type": "Datetime", "key": "modified", "width": "8rem"},
+            ]
+
+        if not rows:
+            rows = ["name"]
+
+        default_view_filters = {
+            "dt": doctype,
+            "type": view_type or 'list',
+            "is_default": 1,
+            "user": frappe.session.user,
+        }
+
+        if not custom_view and frappe.db.exists("CRM View Settings", default_view_filters):
+            list_view_settings = frappe.get_doc("CRM View Settings", default_view_filters)
+            columns = frappe.parse_json(list_view_settings.columns)
+            rows = frappe.parse_json(list_view_settings.rows)
+            is_default = False
+        elif not custom_view or is_default and hasattr(_list, "custom_communication_default_list_data"):
+            rows = default_rows
+            columns = custom_communication_default_list_data().get("columns")
+
+        # check if rows has all keys from columns if not add them
+        for column in columns:
+            if column.get("key") not in rows:
+                rows.append(column.get("key"))
+            column["label"] = _(column.get("label"))
+
+            if column.get("key") == "_liked_by" and column.get("width") == "10rem":
+                column["width"] = "50px"
+
+        # check if rows has group_by_field if not add it
+        if group_by_field and group_by_field not in rows:
+            rows.append(group_by_field)
+
+        data = frappe.get_list(
+            doctype,
+            fields=rows,
+            filters=filters,
+            order_by=order_by,
+            page_length=page_length,
+        ) or []
+
+    if view_type == "kanban":
+        if not rows:
+            rows = default_rows
+
+        if not kanban_columns and column_field:
+            field_meta = frappe.get_meta(doctype).get_field(column_field)
+            if field_meta.fieldtype == "Link":
+                kanban_columns = frappe.get_all(
+                    field_meta.options,
+                    fields=["name"],
+                    order_by="modified asc",
+                )
+            elif field_meta.fieldtype == "Select":
+                kanban_columns = [{"name": option} for option in field_meta.options.split("\n")]
+
+        if not title_field:
+            title_field = "name"
+            if hasattr(_list, "default_kanban_settings"):
+                title_field = _list.default_kanban_settings().get("title_field")
+
+        if title_field not in rows:
+            rows.append(title_field)
+
+        if not kanban_fields:
+            kanban_fields = ["name"]
+            if hasattr(_list, "default_kanban_settings"):
+                kanban_fields = json.loads(_list.default_kanban_settings().get("kanban_fields"))
+
+        for field in kanban_fields:
+            if field not in rows:
+                rows.append(field)
+
+        for kc in kanban_columns:
+            column_filters = { column_field: kc.get('name') }
+            order = kc.get("order")
+            if column_field in filters and filters.get(column_field) != kc.name or kc.get('delete'):
+                column_data = []
+            else:
+                column_filters.update(filters.copy())
+                page_length = 20
+
+                if kc.get("page_length"):
+                    page_length = kc.get("page_length")
+
+                if order:
+                    column_data = get_records_based_on_order(doctype, rows, column_filters, page_length, order)
+                else:
+                    column_data = frappe.get_list(
+                        doctype,
+                        fields=rows,
+                        filters=convert_filter_to_tuple(doctype, column_filters),
+                        order_by=order_by,
+                        page_length=page_length,
+                    )
+
+                new_filters = filters.copy()
+                new_filters.update({ column_field: kc.get('name') })
+
+                all_count = len(frappe.get_list(doctype, filters=convert_filter_to_tuple(doctype, new_filters)))
+
+                kc["all_count"] = all_count
+                kc["count"] = len(column_data)
+
+                for d in column_data:
+                    getCounts(d, doctype)
+
+            if order:
+                column_data = sorted(
+                    column_data, key=lambda x: order.index(x.get("name"))
+                    if x.get("name") in order else len(order)
+                )
+
+            data.append({"column": kc, "fields": kanban_fields, "data": column_data})
+
+    fields = frappe.get_meta(doctype).fields
+    fields = [field for field in fields if field.fieldtype not in no_value_fields]
+    fields = [
+        {
+            "label": _(field.label),
+            "type": field.fieldtype,
+            "value": field.fieldname,
+            "options": field.options,
+        }
+        for field in fields
+        if field.label and field.fieldname
+    ]
+
+    std_fields = [
+        {"label": "Name", "type": "Data", "value": "name"},
+        {"label": "Created On", "type": "Datetime", "value": "creation"},
+        {"label": "Last Modified", "type": "Datetime", "value": "modified"},
+        {
+            "label": "Modified By",
+            "type": "Link",
+            "value": "modified_by",
+            "options": "User",
+        },
+        {"label": "Assigned To", "type": "Link", "value": "_assign", "options": "User"},
+        {"label": "Owner", "type": "Link", "value": "owner", "options": "User"},
+        {"label": "Like", "type": "Data", "value": "_liked_by"},
+    ]
+
+    for field in std_fields:
+        if field.get('value') not in rows:
+            rows.append(field.get('value'))
+        if field not in fields:
+            field["label"] = _(field["label"])
+            fields.append(field)
+
+    if not is_default and custom_view_name:
+        is_default = frappe.db.get_value("CRM View Settings", custom_view_name, "load_default_columns")
+
+    if group_by_field and view_type == "group_by":
+        def get_options(type, options):
+            if type == "Select":
+                return [option for option in options.split("\n")]
+            else:
+                has_empty_values = any([not d.get(group_by_field) for d in data])
+                options = list(set([d.get(group_by_field) for d in data]))
+                options = [u for u in options if u]
+                if has_empty_values:
+                    options.append("")
+
+                if order_by and group_by_field in order_by:
+                    order_by_fields = order_by.split(",")
+                    order_by_fields = [(field.split(" ")[0], field.split(" ")[1]) for field in order_by_fields]
+                    if (group_by_field, "asc") in order_by_fields:
+                        options.sort()
+                    elif (group_by_field, "desc") in order_by_fields:
+                        options.sort(reverse=True)
+                else:
+                    options.sort()
+                return options
+
+        for field in fields:
+            if field.get("value") == group_by_field:
+                group_by_field = {
+                    "label": field.get("label"),
+                    "name": field.get("value"),
+                    "type": field.get("type"),
+                    "options": get_options(field.get("type"), field.get("options")),
+                }
+
+    return {
+        "data": data,
+        "columns": columns,
+        "rows": rows,
+        "fields": fields,
+        "column_field": column_field,
+        "title_field": title_field,
+        "kanban_columns": kanban_columns,
+        "kanban_fields": kanban_fields,
+        "group_by_field": group_by_field,
+        "page_length": page_length,
+        "page_length_count": page_length_count,
+        "is_default": is_default,
+        "views": get_views(doctype),
+        "total_count": len(frappe.get_list(doctype, filters=filters)),
+        "row_count": len(data),
+        "form_script": get_form_script(doctype),
+        "list_script": get_form_script(doctype, "List"),
+        "view_type": view_type,
+    }
 
