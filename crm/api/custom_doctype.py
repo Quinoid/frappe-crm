6bb56e4c25996ee15e1ee6f478f7c97739a08224
Arya <Arya-Dilipan>
2024-12-04 18:14:45 +0530

    get company details

--- crm/api/custom_doctype.py
+++ crm/api/custom_doctype.py
@@ -20,3 +20,33 @@ def get_doctype_data(doctype, name):
     data["_form_script"] = get_form_script(doctype)
     data["_assign"] = get_assigned_users(doctype, data.name, data.owner)
     return data
+
+import frappe
+
+
+@frappe.whitelist()
+def get_default_company():  
+    # Fetch the default company from Global Defaults
+    default_company = frappe.db.get_single_value("Global Defaults", "default_company")
+    return default_company
+
+
+@frappe.whitelist()
+def get_company_details():
+    Doctype = frappe.qb.DocType('Company')
+
+
+    company = frappe.db.get_single_value("Global Defaults", "default_company")
+
+    query = frappe.qb.from_(Doctype).select("*").where(Doctype.name == company).limit(1)
+
+    data = query.run(as_dict=True)
+    if not len(data):
+        frappe.throw(_("data not found"), frappe.DoesNotExistError)
+    data = data.pop()
+
+    data["doctype"] = 'Company'
+    data["fields_meta"] = get_fields_meta('Company')
+    data["_form_script"] = get_form_script('Company')
+    data["_assign"] = get_assigned_users('Company', data.name, data.owner)
+    return data
