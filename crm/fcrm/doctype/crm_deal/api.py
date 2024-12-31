3be0cfb8b45b37e3a2386b362dde068c19de47ec
Arya <Arya-Dilipan>
2024-12-31 12:44:01 +0530

    deal service

--- crm/fcrm/doctype/crm_deal/api.py
+++ crm/fcrm/doctype/crm_deal/api.py
@@ -28,6 +28,17 @@ def get_deal(name):
 	)
 
 	deal["doctype"] = "CRM Deal"
+
+	# Fetch the interested services from the Custom Lead Service table
+    services_query = (
+        frappe.qb.from_(CustomLeadService)
+        .select(CustomLeadService.link_field) 
+        .where(CustomLeadService.parent == name)
+    )
+    services = services_query.run(as_dict=True)
+    
+    deal["interested_services_for_lead"] = [service["link_field"] for service in services]
+
 	deal["fields_meta"] = get_fields_meta("CRM Deal") 
 	deal["_form_script"] = get_form_script('CRM Deal')
 	deal["_assign"] = get_assigned_users("CRM Deal", deal.name, deal.owner)
