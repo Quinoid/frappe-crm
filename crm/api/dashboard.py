100484750f11d51e51a81141fa8257fb4b7495db
Arya <Arya-Dilipan>
2025-03-05 14:54:27 +0530

    sales manager

--- crm/api/dashboard.py
+++ crm/api/dashboard.py
@@ -307,7 +307,7 @@ def get_users_with_roles():
             }
 
         # Rename roles
-        role_name = "Manager" if user["role"] == "Manager Access" else "Regular Access"
+        role_name = "Sales Manager" if user["role"] == "Manager Access" else "Regular Access"
         user_dict[user_id]["roles"].append(role_name)
 
     return list(user_dict.values())
